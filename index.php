1240807dc59cf1285ebfcfb481d4e39a638576d4
Volodymyr Dudyshyn <vovchykd@MacBook-Pro-Volodymyr.local>
2024-04-02 21:47:15 +0300

    codeWars2

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 include __DIR__ . '/vendor/autoload.php';
 
 //include "src/api/random-animal.phtml";
-include "src/codewars/splitString.phtml";
+
+include "src/codeWars/splitString.phtml";
+include "src/codeWars/sentenceSmash.phtml";
